cd145ef03417e6fbe41e6a2866c60db3704107e5
veli akgün <aliveli_ben@hotmail.com>
2022-02-11 19:25:38 +0300

    Add project files.

---
+++ NorthwindDataAccess/Concrete/ProductDal.cs
@@ -0,0 +1,68 @@
+﻿using NorthwindEntities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindDataAccess.Concrete
+{
+    public class ProductDal
+    {
+        //veritabanı ekle sil güncelle işlemlerini burada yapıyoruz.
+        //burada bu kadar iş yapılır
+        public List<Product> GetAll()
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Products.ToList();
+
+            }
+
+        }
+
+        public Product Get(int id)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                return context.Products.SingleOrDefault(p => p.ProductId == id);
+
+            }
+
+        }
+
+        public void Add(Product product)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                context.Products.Add(product);
+                context.SaveChanges();
+
+            }
+
+        }
+        public void Update(Product product)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+
+                
+                //context.Products.Update(product);
+                context.SaveChanges();
+
+            }
+
+        }
+        public void Delete(Product product)
+        {
+            using (NorthwindContext context = new NorthwindContext())
+            {
+                //context.Products.Delete(product);
+                context.SaveChanges();
+
+            }
+
+        }
+
+    }
+}
---
+++ NorthwindEntities/Concrete/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindEntities.Concrete
+{
+    public class Product
+    {
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public string CategoryId  { get; set; }
+        public decimal UnitPrice { get; set; }
+
+        public string QuantityPerUnit { get; set; }
+
+        public Int16 UnitInStock { get; set; }
+    }
+}
